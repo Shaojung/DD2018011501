a3c4ea0c1a44bb2bded3d3fdc472dbacb45fafdc
Teacher <shaojung@gmail.com>
2018-01-15 09:49:08 +0800

    StringRequest

--- app/src/main/java/tw/com/pcschool/dd2018011501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018011501/MainActivity.java
@@ -3,6 +3,7 @@ package tw.com.pcschool.dd2018011501;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,6 +11,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
                         img.setImageBitmap(response);
                     }
                 }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+    }
+    public void click2(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("https://www.mobile01.com/rss/news.xml",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
